bb0288f315dee17b927597f8e788346a4403afb3
Priyanshijain2509 <priyanshi.jain@ongraph.com>
2023-10-05 19:17:49 +0530

    destroy comment is working and now working on create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,35 @@ class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:destroy]
   before_action :correct_user?, only: :destroy
 
-  def create
-    @micropost =  Micropost.find(params[:micropost_id])
-    @comment = @micropost.comments.build(comment_params)
-    @comment.user = current_user
+  # def create
+  #   @micropost =  Micropost.find(params[:micropost_id])
+  #   @comment = @micropost.comments.build(comment_params)
+  #   @comment.user = current_user
 
-    if @comment.save
-      flash[:success] = "Comment created!"
-    else
-      flash[:error]= "Comment can't be created!"
-    end
-    redirect_to request.referrer || root_url
-  end
+  #   if @comment.save
+  #     flash[:success] = "Comment created!"
+  #   else
+  #     flash[:error]= "Comment can't be created!"
+  #   end
+  #   redirect_to request.referrer || root_url
+  # end
 
   def destroy
     @comment.destroy
     flash[:success] = "Comment deleted"
     redirect_to request.referrer || root_url
+  end
+
+
+  def create
+    @micropost =  Micropost.find(params[:micropost_id])
+    @comment = @micropost.comments.build(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js { redirect_to request.referrer || root_url }
+    end
   end
 
   private
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,18 +1,21 @@
-// $(document).ready(function () {
-//   $('.delete-comment').on('click', function (e) {
-//     e.preventDefault();
-//     const micropostId = $(this).data('micropost-id');
-//     const commentId = $(this).data('comment-id');
-    
-//     $.ajax({
-//       type: 'DELETE',
-//       url: `/microposts/${micropostId}/comments/${commentId}`, // Adjust the route as needed
-//       success: function (response) {
-//         $(`.deleteComment[data-comment-id="${commentId}"]`).remove();
-//       }
-//     });
-//   });
-// });
+$(document).ready(function () {
+  $('.createCommentForm').on('submit', function (e) {
+    e.preventDefault(); 
+    const micropostId = $(this).data('micropost-id');
+    const formData = $(this).serialize();
+    const commentElementById = $(`#comment_${id}`);
+
+    $.ajax({
+      type: 'POST',
+      url: `/microposts/${micropostId}/comments`,
+      data: formData,
+      success: function (response) {
+        
+        console.log(response)
+      }
+    });
+  });
+});
 
 
 $(document).ready(function () {
@@ -20,63 +23,15 @@ $(document).ready(function () {
     e.preventDefault();
     const micropostId = $(this).data('micropost-id');
     const commentId = $(this).data('comment-id');
-    // Store a reference to the comment element for removal
-    const commentElement = $(`.deleteComment[data-comment-id="${commentId}"]`);
-    // Send an AJAX request to delete the comment using micropostId and commentId
+    const id = $(this).attr('data-comment-id');
+    const commentElementById = $(`#comment_${id}`);
+
     $.ajax({
       type: 'DELETE',
       url: `/microposts/${micropostId}/comments/${commentId}`,
       success: function (response) {
-        // Handle success, remove the deleted comment from the DOM
-        commentElement.remove();
+        commentElementById.remove();
       }
     });
   });
 });
-
-
-
-
-// $(document).ready(function () {
-//   $('.deleteComment').on('click', function (e) {
-//     e.preventDefault();
-//     const micropostId = $(this).data('micropost-id');
-//     const commentId = $(this).data('comment-id');
-    
-//     // Store a reference to the comment element for removal
-//     const commentElement = $(`.deleteComment[data-comment-id="${commentId}"]`);
-//     const nearestParagraph = $(this).closest('.DestroyComment').find('p');
-//     // Send an AJAX request to delete the comment using micropostId and commentId
-//     $.ajax({
-//       type: 'DELETE',
-//       url: `/microposts/${micropostId}/comments/${commentId}`,
-//       success: function (response) {
-//         // Handle success, remove the nearest parent element of the clicked "Delete" button
-//         commentElement.closest('.DestroyComment').remove(); // Assuming the comment is wrapped in an element with class "comment"
-//         nearestParagraph.remove();
-//       }
-//     });
-//   });
-// });
-
-// $(document).ready(function () {
-//   $('.deleteComment').on('click', function (e) {
-//     e.preventDefault();
-//     const micropostId = $(this).data('micropost-id');
-//     const commentId = $(this).data('comment-id');
-    
-//     // Store a reference to the nearest <p> element for removal
-//     const nearestParagraph = $(this).closest('.DestroyComment').find('p');
-
-//     // Send an AJAX request to delete the comment using micropostId and commentId
-//     $.ajax({
-//       type: 'DELETE',
-//       url: `/microposts/${micropostId}/comments/${commentId}`,
-//       success: function (response) {
-//         // Handle success, remove the nearest paragraph element
-//         nearestParagraph.remove();
-//       }
-//     });
-//   });
-// });
-
